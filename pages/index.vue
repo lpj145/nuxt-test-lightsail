c93af3de4a37488c48cbecd3b7e7f0900bbbd061
Marcos Dantas <marcos.dantas@conexasaude.com.br>
2020-12-24 15:06:31 -0300

    Change default page

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,30 @@
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        lightsail
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="w-full h-screen">
+    <div class="flex justify-center items-center h-screen">
+      <div class="w-1/3">
+        <div class="text-center">
+          <SearchLogo height="124" width="124" class="fill-current text-green-600 mx-auto mb-12" />
+        </div>
+        <label>
+          <input
+            type="search"
+            autofocus
+            placeholder="Pesquisar por algum termo"
+            class="px-3 py-2 border-2 w-full focus:border-blue-500 focus:outline-none"
+          >
+        </label>
+        <div class="text-center mt-5">
+          <button class="px-4 py-2 border bg-blue-700 text-white focus:outline-none rounded">
+            Pesquisar por termo
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
