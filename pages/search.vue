2ba18a1748d642cd3fa5b499270caf1ed891ea05
Marcos Dantas <marcos.dantas@conexasaude.com.br>
2020-12-24 14:57:51 -0300

    End of test

---
+++ pages/search.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
+<template>
+  <div class="w-full h-screen">
+    <div class="flex justify-center items-center h-screen">
+      <div class="w-1/3">
+        <div class="text-center">
+          <SearchLogo height="124" width="124" class="fill-current text-green-600 mx-auto mb-12" />
+        </div>
+        <label>
+          <input
+            type="search"
+            autofocus
+            placeholder="Pesquisar por algum termo"
+            class="px-3 py-2 border-2 w-full focus:border-blue-500 focus:outline-none"
+          >
+        </label>
+        <div class="text-center mt-5">
+          <button class="px-4 py-2 border bg-blue-700 text-white focus:outline-none rounded">
+            Pesquisar por termo
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
